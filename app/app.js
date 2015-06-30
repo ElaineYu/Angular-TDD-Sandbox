a4ec70359914ae0c65ec75ec1cd1bfe6077558ec
Elaine Yu <elaine@monsoonco.com>
2015-06-30 14:54:57 -0700

    adds ngResource and loads services

--- app/app.js
+++ app/app.js
@@ -3,10 +3,15 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngResource',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'pigLatinServiceModule',
+  'weatherServiceModule'
+
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
